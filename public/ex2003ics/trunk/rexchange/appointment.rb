14707320f4155f1cfbcd41271883a8d2602cd585
montague.thomas <montague.thomas@1d9a3599-cf45-0410-a9e6-9e2b6f3e4c45>
2010-05-20 16:46:33 +0000

    Modified RExchange's appointment.rb file to add support for Organizer and Attendees attributes to Appointment class.

--- public/ex2003ics/trunk/rexchange/appointment.rb
+++ public/ex2003ics/trunk/rexchange/appointment.rb
@@ -7,6 +7,7 @@ module RExchange
     
     attribute_mappings :all_day_event => 'urn:schemas:calendar:alldayevent',
       :busy_status => 'urn:schemas:calendar:busystatus',
+	  :cc => 'urn:schemas:httpmail:cc', # Added Support for Attendees
       :contact => 'urn:schemas:calendar:contact',
       :contact_url => 'urn:schemas:calendar:contacturl',
       :created_on => 'urn:schemas:calendar:created',
@@ -16,22 +17,24 @@ module RExchange
       :start_at => 'urn:schemas:calendar:dtstart',
       :duration => 'urn:schemas:calendar:duration',
       :expires_on => 'urn:schemas:calendar:exdate',
-      :expiry_rule => 'urn:schemas:calendar:exrule',          
-      :has_attachment? => 'urn:schemas:httpmail:hasattachment', 
-      :html => 'urn:schemas:httpmail:htmldescription', 
-      :modified_on => 'urn:schemas:calendar:lastmodified', 
-      :location => 'urn:schemas:calendar:location', 
-      :location_url => 'urn:schemas:calendar:locationurl', 
-      :meeting_status => 'urn:schemas:calendar:meetingstatus', 
-      :normalized_subject => 'urn:schemas:httpmail:normalizedsubject', 
-      :priority => 'urn:schemas:httpmail:priority', 
-      :recurres_on => 'urn:schemas:calendar:rdate', 
-      :reminder_offset => 'urn:schemas:calendar:reminderoffset', 
-      :reply_time => 'urn:schemas:calendar:replytime', 
-      :sequence => 'urn:schemas:calendar:sequence', 
-      :subject => 'urn:schemas:httpmail:subject', 
-      :body => 'urn:schemas:httpmail:textdescription', 
-      :timezone => 'urn:schemas:calendar:timezone', 
+      :expiry_rule => 'urn:schemas:calendar:exrule',
+	  :from => 'urn:schemas:httpmail:from', # Added Support for Organizer
+      :has_attachment? => 'urn:schemas:httpmail:hasattachment',
+      :html => 'urn:schemas:httpmail:htmldescription',
+      :modified_on => 'urn:schemas:calendar:lastmodified',
+      :location => 'urn:schemas:calendar:location',
+      :location_url => 'urn:schemas:calendar:locationurl',
+      :meeting_status => 'urn:schemas:calendar:meetingstatus',
+      :normalized_subject => 'urn:schemas:httpmail:normalizedsubject',
+      :priority => 'urn:schemas:httpmail:priority',
+      :recurres_on => 'urn:schemas:calendar:rdate',
+      :reminder_offset => 'urn:schemas:calendar:reminderoffset',
+      :reply_time => 'urn:schemas:calendar:replytime',
+      :sequence => 'urn:schemas:calendar:sequence',
+      :subject => 'urn:schemas:httpmail:subject',
+      :body => 'urn:schemas:httpmail:textdescription',
+      :timezone => 'urn:schemas:calendar:timezone',
+  	  :to => 'urn:schemas:httpmail:to', # Added Support for Attendees
       :uid => 'urn:schemas:calendar:uid'
 
   end
